5ffa2b5dcefe264d6a8ed2c5a145c18488e3100a
manotosh977 <95604480+manotosh977@users.noreply.github.com>
2021-12-06 11:12:13 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##this function makes a matrix which will be a non-singular type 
+makeCacheMatrix <- function(x) {
+        x=matrix()
+        print(x)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+##must be the x non singular 
+##the dimention of x equal or a square matrix
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        if (det(x)!=0){
+                y<-solve(x)
+        }
+        print(y)
 }
